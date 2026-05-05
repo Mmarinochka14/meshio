c7859b1b6a32c413718e83c3a120c36433961033
motyatag <mr.cylllka@bk.ru>
2026-05-05 22:32:59 +0300

    Деплой 2

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -24,12 +24,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-25*_f%0j5pu)p7b%5hjotj^c4(m1*ucr%_sd1=2wqc++!_!&m5'
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "django-insecure-25*_f%0j5pu)p7b%5hjotj^c4(m1*ucr%_sd1=2wqc++!_!&m5",
+)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost",
+).split(",")
 
 
 # Application definition
@@ -140,7 +145,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
